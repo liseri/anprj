85dd105b89c64dcfd8dfea2468a24f1eef217b1b
liseri <lishuai061@126.com>
2016-10-04 22:33:16 +0800

    modify user about

--- src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
+++ src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
@@ -10,7 +10,7 @@
     function UserManagementDialogController ($stateParams, $uibModalInstance, entity, User, JhiLanguageService) {
         var vm = this;
 
-        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN'];
+        vm.authorities = ['ROLE_PUSER','ROLE_EUSER', 'ROLE_ADMIN'];
         vm.clear = clear;
         vm.languages = null;
         vm.save = save;
